5784922fd6a4d24f19b759028d3313deba0d8107
George <40713234+DCel567@users.noreply.github.com>
2020-07-24 11:11:35 +0300

    Add files via upload

--- Handler/MainWindow.xaml.cs
+++ Handler/MainWindow.xaml.cs
@@ -6,57 +6,66 @@ namespace Handler {
     public partial class MainWindow : Window {
 
         DispatcherTimer timer;
-        bool isTimerOn = false;
-        int waitingForSecs;
-        int waitingForMins;
-        int waitingForHours;
+        
 
         public MainWindow() {
             InitializeComponent();
 
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1.0);
-            timer.Start();
-            timer.Tick += new EventHandler( timerTick);
+        }
 
+        private void Start_Button_Click(object sender, RoutedEventArgs e) {
+            
+                Start_Button.IsEnabled = false;
+                Time_Left.IsEnabled = false;
+                Time_Right.IsEnabled = false;
+
+                timer = new DispatcherTimer();
+                timer.Interval = TimeSpan.FromSeconds(1.0);
+                timer.Start();
+                timer.Tick += new EventHandler(timerTick);
+            
         }
 
         private void timerTick(object sender, EventArgs e){
-            DateTime datetime = DateTime.Now;
-            Timer.Text = datetime.ToString("HH:mm:ss");
-            if(isTimerOn == true){
-                if(DateTime.Now.Second == waitingForSecs && DateTime.Now.Minute == waitingForMins){
-                    Timer_Button.IsEnabled = true;
-                }
+            if(Delay.Text == "00:01") {
+                timer.Stop();
+                Start_Button.IsEnabled = true;
+                Time_Left.IsEnabled = true;
+                Time_Right.IsEnabled = true;
             }
+
+            if(timer_secs() == 0){
+                Delay.Text = "" + String.Format("{0:d2}",(timer_mins() - 1)) + ":59";
+            } else {
+                Delay.Text = "" + String.Format("{0:d2}", timer_mins()) + ":" + String.Format("{0:d2}", (timer_secs()-1));
+            }
+
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e){
-            Start_Field.Text = Timer.Text;
-            int delay;
-            isTimerOn = int.TryParse(Delay.Text, out delay);
-
-            if(isTimerOn){
-                Timer_Button.IsEnabled = false;
-                concat_minutes(delay);
-            }
+        private int timer_mins(){
+            bool gotMins = Int32.TryParse(Delay.Text.Substring(0, 3), out int mins);
+            if(!gotMins) Int32.TryParse(Delay.Text.Substring(0, 2), out mins);
+            return mins;
         }
 
-        private void concat_minutes(int delay){
-            waitingForSecs = DateTime.Now.Second ;
-            waitingForMins = DateTime.Now.Minute + delay;
-            waitingForHours = DateTime.Now.Hour;
-            if (waitingForSecs/60 >= 1){
-                waitingForMins += waitingForSecs / 60;
-                waitingForSecs %= 60;
-            }
-            if (waitingForMins/60 >= 1){
-                waitingForHours += waitingForMins / 60;
-                waitingForMins %= 60;
-            }
-            if (waitingForHours/24 >= 1) waitingForHours %= 24;
-
-            Stop_Field.Text = "" + waitingForHours + ":" + waitingForMins + ":" + waitingForSecs;
+        private int timer_secs(){
+            Int32.TryParse(Delay.Text.Substring(Delay.Text.Length-2, 2), out int secs);
+            return secs;
         }
+
+        private void Time_Left_Click(object sender, RoutedEventArgs e) {
+            if(!Time_Right.IsEnabled) Time_Right.IsEnabled = true;
+            int mins = timer_mins();
+            Delay.Text = "" + (mins - 5) + ":" + "00";
+            if(mins - 5 == 10) Time_Left.IsEnabled = false;
+        }
+
+        private void Time_Right_Click(object sender, RoutedEventArgs e){
+            if(!Time_Left.IsEnabled) Time_Left.IsEnabled = true;
+            int mins = timer_mins();
+            Delay.Text = "" + (mins + 5) + ":" + "00";
+            if(mins + 5 == 120) Time_Right.IsEnabled = false;
+        }
+
     }
 }
